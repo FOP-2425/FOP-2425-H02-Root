cbe3ae6862aa6437f67c3f2130b33beda49b11ba
Sebastian Braun <sebastian.braun@stud.tu-darmstadt.de>
2024-06-14 17:02:52 +0200

    feat: Implement win conditions

--- src/main/java/h02/FourWins.java
+++ src/main/java/h02/FourWins.java
@@ -1,6 +1,8 @@
 package h02;
 
+import fopbot.Direction;
 import fopbot.Robot;
+import fopbot.RobotFamily;
 import fopbot.World;
 import h02.template.InputHandler;
 import org.tudalgo.algoutils.student.annotation.DoNotTouch;
@@ -59,4 +61,211 @@ public class FourWins {
         }
         return false;
     }
+
+    /**
+     * Checks if the current player has won by any condition. The conditions can be a horizontal, vertical, diagonal,
+     * or anti-diagonal line of at least four coins.
+     *
+     * @param coins         2D array representing the game board, where each cell contains a RobotFamily
+     *                      color indicating the player that has placed a coin in that position.
+     * @param currentPlayer The RobotFamily color representing the current player to check for a win.
+     * @return true if the current player has formed a horizontal line of at least four coins; false otherwise.
+     */
+    @StudentImplementationRequired("H2.2.3")
+    boolean testWinConditions(final RobotFamily[][] coins, final RobotFamily currentPlayer) {
+        return testWinVertical(coins, currentPlayer) || testWinHorizontal(coins, currentPlayer) || testWinDiagonal(coins, currentPlayer) || testWinAntiDiagonal(coins, currentPlayer);
+    }
+
+    /**
+     * Checks if the current player has won by forming a horizontal line of at least four coins.
+     *
+     * @param coins         2D array representing the game board, where each cell contains a RobotFamily
+     *                      color indicating the player that has placed a coin in that position.
+     * @param currentPlayer The RobotFamily color representing the current player to check for a win.
+     * @return true if the current player has formed a horizontal line of at least four coins; false otherwise.
+     */
+    @StudentImplementationRequired("H2.2.3")
+    boolean testWinHorizontal(final RobotFamily[][] coins, final RobotFamily currentPlayer) {
+        int coinCount = 0;
+        for (int row = 0; row < World.getHeight(); row++) {
+            for (int column = 0; column < World.getWidth(); column++) {
+                if (coins[row][column] == currentPlayer) coinCount++;
+                else coinCount = 0;
+                if (coinCount >= 4) return true;
+            }
+
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the current player has won by forming a vertical line of at least four coins.
+     *
+     * @param coins         2D array representing the game board, where each cell contains a RobotFamily
+     *                      color indicating the player that has placed a coin in that position.
+     * @param currentPlayer The RobotFamily color representing the current player to check for a win.
+     * @return true if the current player has formed a vertical line of at least four coins; false otherwise.
+     */
+    @StudentImplementationRequired("H2.2.3")
+    boolean testWinVertical(final RobotFamily[][] coins, final RobotFamily currentPlayer) {
+        int coinCount = 0;
+        for (int column = 0; column < World.getWidth(); column++) {
+            for (int row = 0; row < World.getHeight(); row++) {
+                if (coins[row][column] == currentPlayer) coinCount++;
+                else coinCount = 0;
+                if (coinCount >= 4) return true;
+            }
+            coinCount = 0;
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the current player has won by forming a diagonal (bottom left to top right) line of at least four coins.
+     *
+     * @param coins         2D array representing the game board, where each cell contains a RobotFamily
+     *                      color indicating the player that has placed a coin in that position.
+     * @param currentPlayer The RobotFamily color representing the current player to check for a win.
+     * @return true if the current player has formed a diagonal line of at least four coins; false otherwise.
+     */
+    @DoNotTouch
+    boolean testWinDiagonal(final RobotFamily[][] coins, final RobotFamily currentPlayer) {
+        int coinCount = 0;
+        final int MAX_COINS = 4;
+
+        final int WIDTH = World.getWidth();
+        final int HEIGHT = World.getHeight();
+
+        final int SMALL_SIDE = Math.min(WIDTH, HEIGHT);
+
+        // upper left triangle
+        for (int i = 0; i < SMALL_SIDE; i++) {
+            for (int j = 0; j < SMALL_SIDE - i; j++) {
+                final int x = j;
+                final int y = HEIGHT - SMALL_SIDE + i + j;
+
+                if (coins[y][x] == currentPlayer) coinCount++;
+                else coinCount = 0;
+                if (coinCount >= MAX_COINS) return true;
+            }
+            coinCount = 0;
+        }
+
+        // center
+        if (WIDTH == SMALL_SIDE) {
+            for (int i = 1; i < HEIGHT - SMALL_SIDE; i++) {
+                for (int j = 0; j < SMALL_SIDE; j++) {
+                    final int x = j;
+                    final int y = i + j;
+
+                    if (coins[y][x] == currentPlayer) coinCount++;
+                    else coinCount = 0;
+                    if (coinCount >= MAX_COINS) return true;
+                }
+                coinCount = 0;
+            }
+        } else {
+            for (int i = 1; i < WIDTH - SMALL_SIDE; i++) {
+                for (int j = 0; j < SMALL_SIDE; j++) {
+                    final int x = i + j;
+                    final int y = j;
+
+                    if (coins[y][x] == currentPlayer) coinCount++;
+                    else coinCount = 0;
+                    if (coinCount >= MAX_COINS) return true;
+                }
+                coinCount = 0;
+            }
+        }
+
+        // lower right triangle
+        for (int i = 0; i < SMALL_SIDE; i++) {
+            for (int j = 0; j < SMALL_SIDE - i; j++) {
+                final int x = WIDTH - 1 - j;
+                final int y = SMALL_SIDE - 1 - (i + j);
+
+                if (coins[y][x] == currentPlayer) coinCount++;
+                else coinCount = 0;
+                if (coinCount >= MAX_COINS) return true;
+            }
+            coinCount = 0;
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks if the current player has won by forming a diagonal (top left to bottom right) line of at least four coins.
+     *
+     * @param coins         2D array representing the game board, where each cell contains a RobotFamily
+     *                      color indicating the player that has placed a coin in that position.
+     * @param currentPlayer The RobotFamily color representing the current player to check for a win.
+     * @return true if the current player has formed a diagonal line of at least four coins; false otherwise.
+     */
+    @DoNotTouch
+    boolean testWinAntiDiagonal(final RobotFamily[][] coins, final RobotFamily currentPlayer) {
+        int coinCount = 0;
+        final int MAX_COINS = 4;
+
+        final int WIDTH = World.getWidth();
+        final int HEIGHT = World.getHeight();
+
+        final int SMALL_SIDE = Math.min(WIDTH, HEIGHT);
+
+        // lower left triangle
+        for (int i = 0; i < SMALL_SIDE; i++) {
+            for (int j = 0; j < SMALL_SIDE - i; j++) {
+                final int x = SMALL_SIDE - 1 - (i + j);
+                final int y = j;
+
+                if (coins[y][x] == currentPlayer) coinCount++;
+                else coinCount = 0;
+                if (coinCount >= MAX_COINS) return true;
+            }
+            coinCount = 0;
+        }
+
+        // center
+        if (WIDTH == SMALL_SIDE) {
+            for (int i = 1; i < HEIGHT - SMALL_SIDE; i++) {
+                for (int j = 0; j < SMALL_SIDE; j++) {
+                    final int x = WIDTH - 1 - j;
+                    final int y = i + j;
+
+                    if (coins[y][x] == currentPlayer) coinCount++;
+                    else coinCount = 0;
+                    if (coinCount >= MAX_COINS) return true;
+                }
+                coinCount = 0;
+            }
+        } else {
+            for (int i = 1; i < WIDTH - SMALL_SIDE; i++) {
+                for (int j = 0; j < SMALL_SIDE; j++) {
+                    final int x = WIDTH - 1 - i - j;
+                    final int y = j;
+
+                    if (coins[y][x] == currentPlayer) coinCount++;
+                    else coinCount = 0;
+                    if (coinCount >= MAX_COINS) return true;
+                }
+                coinCount = 0;
+            }
+        }
+
+        // upper right triangle
+        for (int i = 0; i < SMALL_SIDE; i++) {
+            for (int j = 0; j < SMALL_SIDE - i; j++) {
+                final int x = WIDTH - SMALL_SIDE + i + j;
+                final int y = HEIGHT - 1 - j;
+
+                if (coins[y][x] == currentPlayer) coinCount++;
+                else coinCount = 0;
+                if (coinCount >= MAX_COINS) return true;
+            }
+            coinCount = 0;
+        }
+
+        return false;
+    }
+
 }
